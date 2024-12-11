ba8d8ac92b6d6e4e5e61eca1339be0504226fd52
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-11 08:36:37 -0300

    Create pessoas.py

---
+++ pessoas.py
@@ -0,0 +1,13 @@
+class Pessoa:
+  print("isto é uma classe chamada Pessoa")
+
+idade = 15
+
+def saudacao(self):
+  print("Olá Pessoas!")
+
+#output:15
+print(pessoa.saudacao)
+
+#output:("isto é uma classe chamada Pessoa")
+print(Pessoa.__doc__)
