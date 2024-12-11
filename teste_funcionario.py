88223e19f30514edc555722a74100ecbd411df44
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-11 09:26:05 -0300

    Create teste_funcionario.py

---
+++ teste_funcionario.py
@@ -0,0 +1,13 @@
+from funcionarios import Funcionario
+
+funcionario = Funcionario("matheus CU", "matheusCU@blablabla.com.br")
+
+funcionario.cadastro_hora("jan",300)
+funcionario.cadastro_hora("fev",200)
+funcionario.cadastro_salario_hora("jan",30)
+funcionario.cadastro_salario_hora("fev",30)
+
+print(funcionario)
+
+print(funcionario.calcula_salario("jan))
+print(funcionario.calcula_salario("fev"))
