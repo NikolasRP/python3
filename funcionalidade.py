1c22887362f5fbda2b098ab1edc77bfebc27d3b0
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-11 08:59:04 -0300

    Create funcionalidade.py

---
+++ funcionalidade.py
@@ -0,0 +1,51 @@
+def __init__(self, fab, modelo):
+  self.fabricante = fab
+  self.modelo = modelo
+  self.canal_atual = none
+  self.lista_de_canais = []
+  self.volume = 20
+
+def aumentaVolume(self, valor):
+  if self.volume + valor <= 100:
+    self.volume += valor
+  else:
+    self.volume = 100
+
+def diminuiVolume(self, valor):
+  if self.volume - valor >= 0:
+    self.volume -= valor
+  else:
+    self.volume = 0
+
+def trocaCanal(self, canal):
+  if canal in self.lista_de_canais:
+    self.canal_atual = canal
+
+def sintonizaCanal(canal):
+
+
+  class televisor:
+    def __init__(self, fab, modelo):
+      self.fabricante = fab
+      self.modelo = modelo
+      self.canal_atual = none
+      self.lista_de_canais = []
+      self.volume = 20
+
+def aumentaVolume(self, valor):
+  if self.volume + valor <= 100:
+    self.volume += valor
+  else:
+    self.volume = 100
+
+def diminuiVolume(self, valor):
+  if self.volume - valor >= 0:
+    self.volume -= valor
+  else:
+    self.volume = 0
+
+def trocaCanal(self, canal):
+  if canal in self.lista_de_canais:
+    self.canal_atual = canal
+
+def sintonizaCanal(canal):
